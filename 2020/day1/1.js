27dba04c33143b9df8630687b2d80b4da6814e81
Geoff Gallaway <geoff.gallaway@ocelotconsulting.com>
2020-12-01 16:20:32 -0600

    Move files to directory per day

---
+++ 2020/day1/1.js
@@ -0,0 +1,9 @@
+const data = require('fs').readFileSync('1.dat', 'utf-8').split('\n').filter(n => n).map((e) => Number(e));
+const results = data.filter((d1) =>
+  data.filter((d2) => 
+    data.filter((d3) => 
+      d1 + d2 + d3 == 2020
+    )[0]
+  )[0] !== undefined
+);
+console.log(results.reduce((x, y) => x * y));
