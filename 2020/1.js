878601203513527b014f3b1ee2f256043585eb71
Geoff Gallaway <geoff.gallaway@ocelotconsulting.com>
2020-12-01 15:05:11 -0600

    Day 1 Part 2 complete

--- 2020/1.js
+++ 2020/1.js
@@ -1,8 +1,10 @@
 const data = require('fs').readFileSync('1.dat', 'utf-8').split('\n').filter(n => n).map((e) => Number(e));
 const results = data.filter((d1) =>
   data.filter((d2) => 
-    d1 + d2 == 2020
+    data.filter((d3) => 
+      d1 + d2 + d3 == 2020
+    )[0]
   )[0] !== undefined
 );
 console.log(results)
-console.log(results[0] * results[1]);
+console.log(results.reduce((x, y) => x * y));
