b20b3d53118d787a51dfe890795ae63378e7689a
Geoff Gallaway <geoff.gallaway@ocelotconsulting.com>
2020-12-02 14:27:10 -0600

    Rename

--- 2020/day2/2-1.js
+++
@@ -1,6 +0,0 @@
-const data = require('fs').readFileSync('2.dat', 'utf-8').split('\n').filter(n => n);
-const results = data.filter((data) => {
-  const [firstIndex, secondIndex, chara, pw] = data.split(/[\s-:]+/)
-  return pw.charAt(firstIndex - 1) == chara ^ pw.charAt(secondIndex - 1) == chara
-});
-console.log(results.length)
