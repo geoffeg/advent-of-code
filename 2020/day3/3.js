8e90acb8692141d3767f3eefba836d813c5d73bb
Geoffrey Gallaway <geoffeg@geoffeg.org>
2020-12-02 23:27:38 -0600

    Hmm

--- 2020/day3/3.js
+++ 2020/day3/3.js
@@ -1,7 +1,7 @@
 const lines = require('fs').readFileSync('3.dat', 'utf-8').split('\n').filter(n => n)
 var tobogganPos = 0
 const results = lines.map((line) => {
-  const charPos = line.charAt(tobogganPos) == '' ? tobogganPos - line.length : tobogganPos += 3
+  const charPos = line.charAt(tobogganPos) == '' ? tobogganPos = tobogganPos - line.length : tobogganPos += 3
   return line.charAt(charPos)
 })
 console.log(results)
