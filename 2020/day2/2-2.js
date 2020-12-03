61aed0e5fed33eef1b5986d89fccecd7d08ed7fd
Geoffrey Gallaway <geoffeg@geoffeg.org>
2020-12-02 22:59:43 -0600

    Update day 2 part 2

--- 2020/day2/2-2.js
+++ 2020/day2/2-2.js
@@ -1,6 +1,6 @@
-const data = require('fs').readFileSync('2.dat', 'utf-8').split('\n').filter(n => n);
-const results = data.filter((data) => {
-  const [firstIndex, secondIndex, chara, pw] = data.split(/[\s-:]+/)
-  return pw.charAt(firstIndex - 1) == chara ^ pw.charAt(secondIndex - 1) == chara
+const lines = require('fs').readFileSync('2.dat', 'utf-8').split('\n').filter(n => n);
+const results = lines.filter((line) => {
+  const [firstIndex, secondIndex, char, pw] = line.split(/[\s-:]+/)
+  return pw.charAt(firstIndex - 1) == char ^ pw.charAt(secondIndex - 1) == char
 });
 console.log(results.length)
