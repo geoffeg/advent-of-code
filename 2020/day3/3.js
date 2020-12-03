4ecf10e86a575eea8f5bdce5ab35e82eb5919b67
Geoffrey Gallaway <geoffeg@geoffeg.org>
2020-12-02 23:26:08 -0600

    Start work on day 3

---
+++ 2020/day3/3.js
@@ -0,0 +1,10 @@
+const lines = require('fs').readFileSync('3.dat', 'utf-8').split('\n').filter(n => n)
+var tobogganPos = 0
+const results = lines.map((line) => {
+  const charPos = line.charAt(tobogganPos) == '' ? tobogganPos - line.length : tobogganPos += 3
+  return line.charAt(charPos)
+})
+console.log(results)
+const trees = results.filter(c => c == '#')
+console.log(trees)
+console.log(trees.length)
