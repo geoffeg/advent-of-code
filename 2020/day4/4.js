608e0636e783ce0c3826004893fef6e0ecbb6c4d
Geoff Gallaway <geoff.gallaway@ocelotconsulting.com>
2020-12-04 12:25:18 -0600

    Day 1 part 1 complete

--- 2020/day4/4.js
+++ 2020/day4/4.js
@@ -1,2 +1,18 @@
-const lines = require('fs').readFileSync('4.dat', 'ascii')
-console.log(lines.split(/\n\s*\n/))
+const lines = require('fs').readFileSync('4.dat', 'ascii').split('\n')
+const required = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+const recordSepIndexes = lines.reduce((a, e, i) => {
+  if (e === '')
+    a.push(i)
+  return a
+}, [0])
+const chunks = recordSepIndexes.map((c, i, a) => {
+  const record = lines.slice(c, a[i+1]).join(' ').split(' ')
+  return i === 0 ? record : record.slice(1)
+}).slice(0, -1)
+const passports = chunks.map((chunk) => 
+  Object.fromEntries(chunk.map((s) => s.split(':')))
+)
+const valid = passports.map((passport) =>
+  required.every(v => Object.keys(passport).includes(v))
+)
+console.log(valid.filter(v => v == true).length)
