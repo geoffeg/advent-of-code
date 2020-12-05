da3829cc3b7a04ddb5e42be6b612d90263c40ee3
Geoff Gallaway <geoff.gallaway@ocelotconsulting.com>
2020-12-05 11:26:31 -0600

    Partially working

--- 2020/day5/5.js
+++ 2020/day5/5.js
@@ -2,14 +2,17 @@ const lines = require('fs').readFileSync('sample.dat', 'ascii').split('\n')
 const ROWS = 127
 const COLUMNS = 8
 const operations = {
-  'F' : () => { return 'front' },
-  'B' : () => { return 'back' },
-  'L' : () => { return 'left' },
-  'R' : () => { return 'right' }
+  'F' : (i) => { console.log('F', i); return i[0][1] /= 2, i },
+  'B' : (i) => { console.log('B', i); return i[0][0] = i[0][1] / 2, i },
+  'L' : (i) => { console.log('L', i); return i[1][1] /= 2, i },
+  'R' : (i) => { console.log('R', i); return i[1][0]  = i[1][1] = 2, i }
 }
 const result = lines.map((line) => {
-  return line.split('').map((chara) => {
-    return operations[chara]()
-  })
+  console.log()
+  return line.split('').reduce((a, v, i) => {
+    const res = operations[v](a)
+    console.log('a', a, 'v', v, 'Res', res)
+    return res
+  }, [[0, 127], [0, 8]])
 })
 console.log(result)
