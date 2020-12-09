10c47245fb4fba3983ebcb02cd3c7c7f79116a70
Geoff Gallaway <geoff.gallaway@ocelotconsulting.com>
2020-12-09 10:25:11 -0600

    Add day 6 part 2 fix formatting again

--- 2020/day6/6-2.js
+++ 2020/day6/6-2.js
@@ -2,7 +2,8 @@ const fileData = require('fs').readFileSync('6.dat', 'ascii').trim().split('\n\n
 const results = fileData.map(line => {
   const allGroupAnswers = line.replace(/\n/g, '')
   const answerFreq = allGroupAnswers.split('').reduce((acc, curr) => { 
-    acc[curr] ? acc[curr] += 1 : acc[curr] = 1; return acc
+    acc[curr] ? acc[curr] += 1 : acc[curr] = 1
+    return acc
   }, {})
   const groupLength = line.split('\n').length
   return Object.entries(answerFreq).filter(f => f[1] === groupLength).map(e => e[0]).length
