6868b25892e18da9556e7eda752eab29e8f299b6
Geoffrey Gallaway <geoffeg@geoffeg.org>
2021-03-02 20:21:03 -0600

    Day 8 Part 2 in Clojure

--- 2020/day8/8.clj
+++ 2020/day8/8.clj
@@ -7,21 +7,31 @@
 
 (defn parse-file [file]
   (mapv parse-line
-       (str/split-lines
-         (slurp file))))
+        (str/split-lines
+          (slurp file))))
 
 (defn run-instructions [instructions]
   (loop [acc 0, line 0, seen #{}]
-    (let [[instruction offset] (instructions line)]
-      (prn instruction offset line)
-      (if (contains? seen line)
-        acc
-        (case instruction
-          "acc" (recur (+ acc offset) (inc line) (conj seen line))
-          "jmp" (recur acc (+ line offset) (conj seen line))
-          "nop" (recur acc (inc line) (conj seen line)))))))
+    (cond (contains? seen line) [acc line]
+          (= line (count instructions)) [acc line]
+          :else (let [[instruction offset] (instructions line)]
+                  (case instruction
+                    "acc" (recur (+ acc offset) (inc line) (conj seen line))
+                    "jmp" (recur acc (+ line offset) (conj seen line))
+                    "nop" (recur acc (inc line) (conj seen line)))))))
 
 (defn part1 [filename]
   (->> filename
        parse-file
-       run-instructions))
+       run-instructions
+       first))
+
+(defn part2 [filename]
+  (let [instructions (parse-file filename)]
+    (->> instructions
+         count
+         range
+         (map #(update-in instructions [% 0] {"jmp" "nop", "nop" "jmp", "acc" "acc"}))
+         (map run-instructions)
+         (filter #(= (count instructions) (last %)))
+         ffirst)))
