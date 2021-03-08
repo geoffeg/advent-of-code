fa7d55b62afd8c66b0138d6e175c0ded6d87650b
Geoffrey Gallaway <geoffeg@geoffeg.org>
2021-03-08 15:07:39 -0600

    Day 9 part 2 updated

--- 2020/day9/9.clj
+++ 2020/day9/9.clj
@@ -8,7 +8,7 @@
   (filter #(= (+ (first %) (last %)) target)
           (for [x items y items] (vector x y))))
 
-(defn find-invalid-numbers [size numbers]
+(defn find-invalid-number [size numbers]
   (->> numbers
        (map #(vector %2 %1) (range))
        (drop size)
@@ -32,20 +32,26 @@
 
 (defn part1 [filename size]
   (let [data (parse-file filename)]
-    (->> data
-         (find-invalid-numbers size))))
+    (find-invalid-number size data)))
 
+; Find which consecutive numbers equal the invalid number, looking at a minimum of 2 numbers
+; To create the list of consecutive numbers to consider, start with a 2 number-wide range of
+; windows starting at the top of the list and going down, then go to 3, 4, 5 wide, etc
+; stopping at the actual width of the list
+; TODO: Rewrite using some of approach that allows lazily generating the list instead of
+; TODO: generating it at the beginning
 ; 0 1 2 3 4 5
 ; ^   ^        start: 0 end: 2 (width: 2)
 ;   ^  ^       start: 1 end: 3 (width: 2)
 ;     ^  ^     start: 2 end: 4 (width: 2)
 ;       ^   ^  start: 3 end: 5 (width: 2)
 ; ^     ^      start: 0 end: 3 (width: 3)
-; [0 2] [1 3] [2 4] [3 5] [0 3] [1 4] [2 5] [0 4] [1 5]
+;   ^     ^    start: 1 end: 4 (width: 3)
+; [0 2] [1 3] [2 4] [3 5] [0 3] [1 4] [2 5] [0 4] [1 5], etc
 
 (defn part2 [filename size]
   (let [data (parse-file filename)
-        invalid-numbers (find-invalid-numbers size data)
+        invalid-numbers (find-invalid-number size data)
         found-range (first (find-total data invalid-numbers))
         range-data (sort (subvec data (first found-range) (last found-range)))]
     (+ (first range-data) (last range-data))))
