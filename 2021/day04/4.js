e1696c68edb503d98c6f6620c2b8e1d3f1268634
Geoffrey Gallaway <geoffeg@geoffeg.org>
2023-05-07 18:24:18 +0000

    Improved day 4 part 1

--- 2021/day04/4.js
+++ 2021/day04/4.js
@@ -7,22 +7,19 @@ const isBoardWinner = (board, drawings) => {
 }
 
 function findWinningBoard(drawings, boards)  {
-    let winningBoard;
-    let winningDrawing = [];
-    drawings.find((drawing, i) => {
-        winningDrawing.push(drawing)
-        const f = boards.find(board => {
-            const horiz = isBoardWinner(board, winningDrawing)
-            const vert = isBoardWinner(board[0].map((_, i) => board.map(row => row[i])), winningDrawing)
+    return drawings.reduce((prev, drawing) => {
+        if (prev.board) return prev
+        prev.drawings.push(drawing)
+        prev.board = boards.find(board => {
+            const horiz = isBoardWinner(board, prev.drawings)
+            const vert = isBoardWinner(board[0].map((_, i) => board.map(row => row[i])), prev.drawings)
             return horiz || vert
         })
-        winningBoard = f;
-        return f != undefined
-    })
-    return { winningDrawing: winningDrawing, winningBoard: winningBoard }
+        return prev
+    }, {drawings: [], board: null})
 }
 
 const winner = findWinningBoard(drawings, boards)
-const boardTotal = winner.winningBoard.flat().filter(boardValue => !winner.winningDrawing.includes(boardValue)).reduce((acc, cur) => acc += parseInt(cur), 0)
-const score = boardTotal * winner.winningDrawing.reverse()[0]
+const boardTotal = winner.board.flat().filter(boardValue => !winner.drawings.includes(boardValue)).reduce((acc, cur) => acc += parseInt(cur), 0)
+const score = boardTotal * winner.drawings.reverse()[0]
 console.log(score)
