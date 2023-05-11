294ce224dc3ef97b7b492625886d235fac003c00
Geoffrey Gallaway <geoffeg@geoffeg.org>
2023-05-11 17:51:38 +0000

    2021 Day 5 part 1 working, still needs cleanup.

---
+++ 2021/day05/5.js
@@ -0,0 +1,35 @@
+const R = require("ramda")
+const data = require('fs').readFileSync(process.argv[2], 'utf-8').split('\n').map(line => line.split(' -> ').map(coords => coords.split(',').map(str => parseInt(str))))
+// * the first value is the column (vertical)
+// * the second value is the row (horizontal)
+function range(start, stop) {
+    const length = Math.abs(stop - start) + 1
+    return Array.from({ length }, (_, i) => {
+      return start > stop ? start - i : start + i
+    })
+}
+
+function initializeGrid(lines) {
+    const columnsMax = lines.reduce((winner, [point1, point2]) => {
+        return Math.max(winner, point1[0], point2[0])
+    }, 0)
+    const rowsMax = lines.reduce((winner, [point1, point2]) => {
+        return Math.max(winner, point1[1], point2[1])
+    }, 0)
+    return Array(rowsMax + 1).fill().map(() => Array(columnsMax + 1).fill(0))
+}
+
+function drawLine(board, point1, point2) {
+    R.xprod(range(point1[0], point2[0]), range(point1[1], point2[1])).forEach(([x, y]) => {
+        board[y][x]++
+    })
+    return board
+}
+  
+const board = initializeGrid(data)
+
+const completedBoard = data.filter(([p1, p2]) => p1[0] == p2[0] || p1[1] == p2[1]).reduce((board, line) => {
+    return drawLine(board, line[0], line[1])
+}, board)
+
+console.log(completedBoard.flat().filter(v => v >= 2).length)
